8a4192f28f7f9285b3d024ff91c310354bfc3cf0
Zu3zz <zthzththth@163.com>
2020-07-13 11:29:39 +0800

    feat: finish coupon-disctribution starter app and config yml

---
+++ coupon-service/coupon-distribution/src/main/java/com/zth/coupon/DistributionApplication.java
@@ -0,0 +1,33 @@
+package com.zth.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author 3zZ.
+ * Date: 2020/7/13.
+ * 分发系统的启动入口
+ */
+@EnableJpaAuditing
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableEurekaClient
+@SpringBootApplication
+public class DistributionApplication {
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributionApplication.class, args);
+    }
+}
